e905854210d8ea1e22d3a0514219b3d23e72648f
combatstone <55021776+combatstone@users.noreply.github.com>
2025-09-04 01:21:15 -0400

    Fixes for server startup and Windows support

--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
   let capturedJsonResponse: Record<string, any> | undefined = undefined;
 
   const originalResJson = res.json;
-  res.json = function (bodyJson, ...args) {
+  res.json = function(bodyJson, ...args) {
     capturedJsonResponse = bodyJson;
     return originalResJson.apply(res, [bodyJson, ...args]);
   };
@@ -59,13 +59,8 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
+  const port = process.env.PORT || 5000;
+  server.listen(port, () => {
+    log(`API Server listening on port ${port}`);
   });
 })();
